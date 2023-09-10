6b499177f7ac97b0d7a395c50f9993096e3e0862
Robert Facella <rob.facella@gmail.com>
2023-09-10 16:35:39 -0400

    random but plays once on launch

--- MainActivity.kt
+++ MainActivity.kt
@@ -13,6 +13,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -37,10 +41,18 @@ class MainActivity : ComponentActivity() {
     }
 }
 @Composable
-fun YeetButton(onClick: () -> Unit) {
+fun YeetButton(onClick: () -> Unit, modifier: Modifier = Modifier) {
     val localContext = LocalContext.current
-    val randomNumber = (0..3).random()
-    val audioPlayer : MediaPlayer
+    //onClick = (0..3).random()
+    var randomNumber by remember { mutableStateOf( 0) }
+    var audioPlayer : MediaPlayer
+
+    ElevatedButton(
+        onClick = { randomNumber = (0..3).random() },
+
+    ) {
+        Text("Y33t!")
+    }
     if (randomNumber == 0 ){
         audioPlayer = MediaPlayer.create(localContext, R.raw.yeet1)
     }
@@ -53,9 +65,7 @@ fun YeetButton(onClick: () -> Unit) {
     else {
         audioPlayer = MediaPlayer.create(localContext, R.raw.yeet4)
     }
-    ElevatedButton(onClick = { audioPlayer.start() }) {
-        Text("Y33t!")
-    }
+    audioPlayer.start()
 
 }
 @Composable
@@ -68,12 +78,11 @@ fun Greeting(greeting: String, url: String, modifier: Modifier = Modifier) {
             modifier = modifier,
             fontSize = 100.sp,
             lineHeight = 111.sp,
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Center,
+            color = Color.Green
 
         )
-        YeetButton {
-
-        }
+        YeetButton(onClick = {  })
         Text(
             text = "$url",
             modifier = Modifier.width(500.dp),
